ab9f815aea23a73a9b90809e7e1d59575d832993
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-02-03 08:18:17 +0700

    Adjust alert color on error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -204,7 +204,7 @@
 
                         @if (session('error'))
 
-                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
